06630bfb5366dac749a882081089f5e79690ad1a
Jonathan Thomas <jonathanthomas@Jonathans-MBP.hsd1.wa.comcast.net>
2018-01-16 13:19:54 -0800

    Laptop work

---
+++ MineGame/Assets/Scripts/Init.cs
@@ -0,0 +1,9 @@
+//Use this file for handling the initalization tasks
+//for the game to behave properly. Likely means handlers, 
+//setting up base stats, and loading any kind of critical files. 
+
+public class Init:MonoBehaviour {
+	public void Awake() {
+
+	}
+}
---
+++ MineGame/Assets/Scripts/Top.cs
@@ -0,0 +1,21 @@
+﻿//Used for storing constants and refereces to important data that needs to
+//exist in a global context. 
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Top : MonoBehaviour {
+
+	public delegate void GenericEvent();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
